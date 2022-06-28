65827259dc5c0d6df42030889bda09bcefeed893
xiaoxiaosuaxuan <xuyuhangtmx@sjtu.edu.cn>
2022-06-29 00:29:48 +0800

    this is a try to commit

---
+++ test.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+print(torch.cuda.is_available())
